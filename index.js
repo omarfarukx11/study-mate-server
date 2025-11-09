72d7854752340767cde9897e2f4bed51e95b8bf9
Omar Faruk <khfarukcht@mail.com>
2025-11-10 00:39:12 +0600

    mongodb conect

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000;
+
+const uri = "mongodb+srv://study_partner:tWOPAy4Haw63qMCn@cluster0.oyyjehq.mongodb.net/?appName=Cluster0";
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("smart server is running");
+});
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+
+    
+    const db = client.db("studyPartner_db")
+    const partnersCollection = db.collection("studyPartner")
+
+     app.get('/studyPartner', async (req, res) => {
+      const cursor = partnersCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+      
+    }
+    )
+
+    app.post("/studyPartner", async(req , res) => {
+        const newPartner = req.body;
+        const result = await partnersCollection.findOne();
+        res.send(result)
+    })
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
